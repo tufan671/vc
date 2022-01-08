ee76772110b39c776a41fd3f58ae12ece1bba5f0
tufan671 <65157420+tufan671@users.noreply.github.com>
2022-01-08 03:57:54 -0800

    Add files via upload

---
+++ src/handlers/functionHandler.js
@@ -0,0 +1,127 @@
+const { GuildMember, TextChannel, MessageEmbed, Guild } = require("discord.js");
+const { numEmojis, coin: coinEmoji } = require("../configs/emojis.json");
+const conf = require("../configs/config.json");
+const penals = require("../schemas/penals");
+const task = require("../schemas/task");
+const coin = require("../schemas/coin");
+
+/**
+ * @param {Client} client
+ */
+module.exports = async (client) => {
+  /**
+   * @param {Number} num
+   * @returns {String}
+   */
+  client.numEmoji = (num) => num.toString().split("").map((x) => numEmojis[x] || x).join("");
+
+  /**
+   * @param {Number} ms
+   * @returns {Promise<unknown>}
+   */
+  client.wait = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+  /**
+   * @param {String|Array} roles
+   * @returns {Promise<any>}
+   */
+  GuildMember.prototype.setRoles = async function (roles) {
+    if (!this.manageable) return;
+    const newRoles = this.roles.cache.filter(x => x.managed).map(x => x.id).concat(roles);
+    return this.roles.set(newRoles).catch(() => {});
+  };
+
+  /**
+   * @param {String} guildID
+   * @param {String} type
+   * @param {Number} data
+   * @param {TextChannel|VoiceChannel} channel
+   * @returns {Promise<void>}
+   */
+  GuildMember.prototype.updateTask = async function (guildID, type, data, channel = null) {
+    const taskData = await task.find({ guildID, userID: this.user.id, type, active: true });
+    taskData.forEach(async (x) => {
+      if (channel && x.channels && x.channels.some((x) => x !== channel.id)) return;
+      x.completedCount += data;
+      if (x.completedCount >= x.count) {
+        x.active = false;
+        x.completed = true;
+        await coin.findOneAndUpdate({ guildID, userID: this.user.id }, { $inc: { coin: x.prizeCount } });
+
+        const embed = new MessageEmbed().setColor(this.displayHexColor).setAuthor(this.displayName, this.user.avatarURL({ dynamic: true, size: 2048 })).setThumbnail("https://img.itch.zone/aW1nLzIzNzE5MzEuZ2lm/original/GcEpW9.gif");
+        if (channel && channel.type === "text") channel.send(embed.setDescription(`
+				${this.toString()} Tebrikler! ${type.charAt(0).toLocaleUpperCase() + type.slice(1)} görevini başarıyla tamamladın.
+				
+				${x.message}
+				${coinEmoji} \`${x.prizeCount} coin kazandın!\`
+				`));
+      }
+      await x.save();
+    });
+  };
+
+  /**
+   * @param {MessageEmbed} embed
+   */
+  TextChannel.prototype.sendEmbed = function (embed) {
+    if (!embed || !embed.description) return;
+    const text = embed.description;
+    for (var i = 0; i < Math.floor(text.length / 2048) + 1; i++) {
+      this.send(embed.setDescription(text.slice(i * 2048, (i + 1) * 2048)));
+    }
+  };
+
+  /**
+   * @param {Message} message
+   * @returns {Promise<Message>}
+   */
+  TextChannel.prototype.wsend = async function (message) {
+    const hooks = await this.fetchWebhooks();
+    let webhook = hooks.find(a => a.name === client.user.username && a.owner.id === client.user.id);
+    if (webhook) return webhook.send(message);
+    webhook = await this.createWebhook(client.user.username, { avatar: client.user.avatarURL() });
+    return webhook.send(message);
+  };
+
+  /**
+   * @param {Message} message
+   * @param {String} text
+   * @returns {Promise<void>}
+   */
+  TextChannel.prototype.error = async function (message, text) {
+    const theark = await client.users.fetch("589079197906108416");
+    const embed = new MessageEmbed()
+      .setColor("RED")
+      .setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true, size: 2048 }))
+      .setFooter("Arcenio ❤️ Valentina", theark.avatarURL({ dynamic: true }));
+    this.send(embed.setDescription(text)).then((x) => { if (x.deletable) x.delete({ timeout: 10000 }); });
+  };
+
+  /**
+   * @returns {Promise<void>}
+   */
+  
+
+  /**
+   * @param {String} guildID
+   * @param {String} userID
+   * @param {String} type
+   * @param {Boolean} active
+   * @param {String} staff
+   * @param {String} reason
+   * @param {Boolean} temp
+   * @param {Number} finishDate
+   * @returns {Promise<Document<any, any>>}
+   */
+  client.penalize = async (guildID, userID, type, active = true, staff, reason, temp = false, finishDate = undefined) => {
+    const id = await penals.find({ guildID });
+    return await new penals({ id: id ? id.length + 1 : 1, userID, guildID, type, active, staff, reason, temp, finishDate }).save();
+  };
+
+  /**
+   * @returns {any}
+   */
+  Array.prototype.random = function () {
+    return this[Math.floor((Math.random() * this.length))];
+  };
+};
